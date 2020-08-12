16fb1383bfc03e02dc1bb57de2ef2c99ed5322c1
Hostej <unknowhostej@gmail.com>
2020-08-12 16:50:00 +0600

    Avatar's with cloudinary

---
+++ src/main/java/kz/xaw/ovaanimerp/repository/PhotoRepository.java
@@ -0,0 +1,7 @@
+package kz.xaw.ovaanimerp.repository;
+
+import kz.xaw.ovaanimerp.data.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+}
